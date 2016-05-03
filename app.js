ca94d0ff429deb0cb2924a017b41930c4e45f710
Ciaran McManus <ciaranmcmanus@live.ie>
2016-05-03 17:53:01 +0100

    Adding the basic server communication pieces.

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
+//file communication using express
+//package communication using socket.io
 
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
 
+//if the query is empty, the index file will be hit
+app.get('/', function(req,res) {
+	res.sendFile(_dirname + '/client/index.html');
+});
+
+//if the route is client, hit the client
+app.use('/client',express.static(_dirname + '/client'));
+
+//listen on port 2000
+serv.listen(2000);
