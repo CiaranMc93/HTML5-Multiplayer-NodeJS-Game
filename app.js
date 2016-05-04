d349da7332a71213aa10a02d6682e3241599240c
Ciaran McManus <ciaranmcmanus@live.ie>
2016-05-04 07:47:31 +0100

    Adding socket functionality that will show 2 players on the screen at once.

--- app.js
+++ app.js
@@ -7,11 +7,60 @@ var serv = require('http').Server(app);
 
 //if the query is empty, the index file will be hit
 app.get('/', function(req,res) {
-	res.sendFile(_dirname + '/client/index.html');
+	res.sendFile(__dirname + '/client/index.html');
 });
 
 //if the route is client, hit the client
-app.use('/client',express.static(_dirname + '/client'));
+app.use('/client',express.static(__dirname + '/client'));
 
 //listen on port 2000
 serv.listen(2000);
+console.log('Server Started');
+
+//create a list of sockets and assign unique ID to each
+var SOCKET_LIST = {};
+
+
+//socket.io
+//loads and init the file and returns an object
+var io = require('socket.io')(serv,{});
+
+
+io.sockets.on('connection', function(socket){
+	//add identifiers for each player
+	socket.id = Math.random();
+	socket.x = 0;
+	socket.y = 0;
+
+	//add to the socket list
+	SOCKET_LIST[socket.id] = socket;
+
+	socket.on('happy', function(){
+		console.log('Happy');
+	});
+});
+
+//loop through socket list
+setInterval(function()
+{
+	var pack = [];
+
+	for(var i in SOCKET_LIST)
+	{
+		var socket = SOCKET_LIST[i];
+		socket.x++;
+		socket.y++;
+		pack.push({
+			x:socket.x,
+			y:socket.y
+		});
+		
+	}
+	//for each player, send the package
+	for(var i in SOCKET_LIST)
+	{
+		var socket = SOCKET_LIST[i];
+		socket.emit('New Position',pack);
+	}
+
+},1000/25);
