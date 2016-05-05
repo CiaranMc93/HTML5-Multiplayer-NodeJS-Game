dd7db523c85da4c353a2ce0355f0a0a39c97b0cc
Ciaran McManus <ciaranmcmanus@live.ie>
2016-05-05 07:41:02 +0100

    Adding movement for each player and disconnect functionality.

--- app.js
+++ app.js
@@ -19,8 +19,44 @@ console.log('Server Started');
 
 //create a list of sockets and assign unique ID to each
 var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = function(id) {
+	var self = {
+		x:250,
+		y:250,
+		id:id,
+		number:"" + Math.floor(10 * Math.random()),
+		moveRight:false,
+		moveLeft:false,
+		moveUp:false,
+		moveDown:false,
+		maxSpd:10,
+	}
+
+	self.updateEntityPosition = function(){
+		//move the player appropriately
+		if(self.moveRight)
+		{
+			self.x += self.maxSpd;
+		}
+		if(self.moveLeft)
+		{
+			self.x -= self.maxSpd;
+		}
+		if(self.moveDown)
+		{
+			self.y += self.maxSpd;
+		}
+		if(self.moveUp)
+		{
+			self.y -= self.maxSpd;
+		}
+	}
 
 
+	return self;
+}
 //socket.io
 //loads and init the file and returns an object
 var io = require('socket.io')(serv,{});
@@ -29,14 +65,31 @@ var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
 	//add identifiers for each player
 	socket.id = Math.random();
-	socket.x = 0;
-	socket.y = 0;
 
+	var player = Player(socket.id);
+	
 	//add to the socket list
 	SOCKET_LIST[socket.id] = socket;
+	//add player to player list
+	PLAYER_LIST[socket.id] = player;
 
-	socket.on('happy', function(){
-		console.log('Happy');
+	//when there is a disconnect
+	socket.on('disconnect', function() {
+		delete SOCKET_LIST[socket.id];
+		delete PLAYER_LIST[socket.id];
+	});
+
+	//when there is movement change
+	socket.on('keyPress', function(data) {
+		//check if the data sent back is the direction and chnage the state accordingly
+		if(data.inputId === 'left')
+			player.moveLeft = data.state;
+		else if(data.inputId === 'right')
+			player.moveRight = data.state;
+		else if(data.inputId === 'up')
+			player.moveUp = data.state;
+		else if(data.inputId === 'down')
+			player.moveDown = data.state;
 	});
 });
 
@@ -45,14 +98,16 @@ setInterval(function()
 {
 	var pack = [];
 
-	for(var i in SOCKET_LIST)
+	for(var i in PLAYER_LIST)
 	{
-		var socket = SOCKET_LIST[i];
-		socket.x++;
-		socket.y++;
+		var player = PLAYER_LIST[i];
+		//update the players position
+		player.updateEntityPosition();
+
 		pack.push({
-			x:socket.x,
-			y:socket.y
+			x:player.x,
+			y:player.y,
+			number:player.number
 		});
 		
 	}
